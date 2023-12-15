fac58501a337108a5f3aed56bf4449d0afbe3fb3
Jade Stewart <jadekstewart3@gmail.com>
2023-12-15 09:11:48 -0700

    Regex notes

--- 7_chapter/sandbox.js
+++ 7_chapter/sandbox.js
@@ -15,3 +15,28 @@ form.addEventListener('submit', e => {
 //attaching a submit event will submit the form even if the user just hits enter
 //submit events by default just refresh the browser
 //can use dot notation to get the value of the input with the id or name of the input
+
+//Regular Expressions
+//regex101.com
+//validations for forms
+//^ start of expression
+//$ end of expression
+//[] character set
+//{1,8} range of characters
+// . any character allows special characters too
+// + one or more characters
+// \ escape character
+// \w word character
+// \d digit character
+// \s whitespace character
+// \t tab character
+// \n newline character
+// \W non-word character
+// \D non-digit character
+// \S non-whitespace character
+// ? optional character
+// | or operator
+// () group characters together
+// i case insensitive modifier
+// g global search modifier
+

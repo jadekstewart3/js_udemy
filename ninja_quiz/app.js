4e7efafecd1780285caf78a567655acf22886fe4
Jade Stewart <jadekstewart3@gmail.com>
2023-12-20 12:22:58 -0700

    Add display of ninja score

--- ninja_quiz/app.js
+++ ninja_quiz/app.js
@@ -6,13 +6,16 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   let score = 0;
   const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
-
+  const result = document.querySelector('.result');
+  
   // check answers
   userAnswers.forEach((answer, index)=> {
     if(answer === correctAnswers[index]){
       score += 25;
     }
   });
-
-  console.log(score);
+  //show result on page
+  scrollTo(0,0); //scroll to top of page
+  result.querySelector('span').textContent = `${score}%`;
+  result.classList.remove('d-none');
 });

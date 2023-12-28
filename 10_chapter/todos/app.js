521f0acae43b6ed5b99f1f5c28a3a22e5be71da6
Jade Stewart <jadekstewart3@gmail.com>
2023-12-28 11:59:24 -0700

    Implement JS functionality to add todos and add simple error handling

---
+++ 10_chapter/todos/app.js
@@ -0,0 +1,25 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = todo => {
+
+  const html = `
+   <li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>`;
+  list.innerHTML += html;
+  
+};
+
+addForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  if(todo.length){
+    generateTemplate(todo);
+    addForm.reset();
+    // resets all of the input fields in the form
+  } else {
+    alert('Please enter a valid todo');
+  }
+});

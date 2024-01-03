b5862e143cc19f8d158c021422fc2aa49f4f4e90
Jade Stewart <jadekstewart3@gmail.com>
2024-01-02 17:44:12 -0700

    Notes about datefns library

--- 11_chapter/sandbox.js
+++ 11_chapter/sandbox.js
@@ -1,45 +1,57 @@
 //dates & times
-const before = new Date('February 1 2019 7:30:59');
-const now = new Date();
+// const before = new Date('February 1 2019 7:30:59');
+// const now = new Date();
 //new keyword goes to the constructor
 
 //year, months, day, times
 
-console.log("getFullYear:", now.getFullYear());
-console.log("getMonth:", now.getMonth());
+// console.log("getFullYear:", now.getFullYear());
+// console.log("getMonth:", now.getMonth());
 //month is zero based
 //returns 0-11
-console.log("getDate:", now.getDate());
-console.log("getDay:", now.getDay());
-console.log("getHours:", now.getHours());
-console.log("getMinutes:", now.getMinutes());
-console.log("getSeconds:", now.getSeconds());
+// console.log("getDate:", now.getDate());
+// console.log("getDay:", now.getDay());
+// console.log("getHours:", now.getHours());
+// console.log("getMinutes:", now.getMinutes());
+// console.log("getSeconds:", now.getSeconds());
 
 
 //timestamps
-console.log("timestamp:", now.getTime());
+// console.log("timestamp:", now.getTime());
 //milliseconds since jan 1st 1970
 
 //date strings
-console.log(now.toDateString());
-console.log(now.toTimeString());
-console.log(now.toLocaleString());
+// console.log(now.toDateString());
+// console.log(now.toTimeString());
+// console.log(now.toLocaleString());
 
 
-const diff = now.getTime() - before.getTime();
+// const diff = now.getTime() - before.getTime();
 
-console.log(diff);
+// console.log(diff);
 
 //time conversion
 
-const mins = Math.round(diff / 1000 / 60);
-console.log(mins);
-const hours = Math.round(mins / 60);
-console.log(hours);
-const days = Math.round(hours / 24);
-console.log(days);
+// const mins = Math.round(diff / 1000 / 60);
+// console.log(mins);
+// const hours = Math.round(mins / 60);
+// console.log(hours);
+// const days = Math.round(hours / 24);
+// console.log(days);
 
 //converting timestamps into date objects
 
-const timestamp = 1675938474990;
-console.log(new Date(timestamp));
+// const timestamp = 1675938474990;
+// console.log(new Date(timestamp));
+
+//date fns library
+
+const now = new Date();
+
+// console.log(datefns.isToday(now));
+
+//formatting options
+
+console.log(datefns.format(now, 'YYYY')); //year
+console.log(datefns.format(now, 'MMMM')); //month name
+// check docs its pretty self explanatory

44704f12801ad313959aabf6a5785133cc9015c2
Jade Stewart <jadekstewart3@gmail.com>
2024-01-10 15:19:14 -0700

    Notes on making an HTTP request

--- 12_chapter/sandbox.js
+++ 12_chapter/sandbox.js
@@ -10,3 +10,21 @@
 // Make HTTP requests to get data from another server
 
 // We make these requests to the API endpoints
+
+// Making HTTP requests  (XHR)
+
+const request = new XMLHttpRequest();
+// XMLHttpRequest is built into the javascript language
+
+request.addEventListener("readystatechange", () => {
+  // console.log(request, request.readyState);
+  if(request.readyState === 4){
+    console.log(request.responseText);
+  }
+});
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+//first argument is the type of request we want to make
+// second argument is the url we want to make the request to
+//kind of like establishing a connection in ruby
+request.send();
+// sends the request to the server

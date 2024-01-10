cb49086cb33cd6b25e745042912f17824c096f32
Jade Stewart <jadekstewart3@gmail.com>
2024-01-10 15:05:18 -0700

    Request notes:

--- 12_chapter/sandbox.js
+++ 12_chapter/sandbox.js
@@ -1,2 +1,12 @@
 // Async JavaScript
 // Governs how we perform tasks which take some time to coplete(like getting data from a database)
+// start something now and finish it later
+
+// Synchronous code
+// JavaScript can run ONE statement at a time
+
+// What are HTTP requests?
+
+// Make HTTP requests to get data from another server
+
+// We make these requests to the API endpoints

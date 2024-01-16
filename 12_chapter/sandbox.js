d77f41ae197f3746d1e2dbedf24350b8298451f4
Jade Stewart <jadekstewart3@gmail.com>
2024-01-16 15:52:54 -0700

    Add additional safegaurd for request

--- 12_chapter/sandbox.js
+++ 12_chapter/sandbox.js
@@ -18,7 +18,7 @@ const request = new XMLHttpRequest();
 
 request.addEventListener("readystatechange", () => {
   // console.log(request, request.readyState);
-  if(request.readyState === 4){
+  if(request.readyState === 4 && request.status === 200){
     console.log(request.responseText);
   }
 });

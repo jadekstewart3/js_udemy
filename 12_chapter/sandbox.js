406809cd954fee4a9e49f253c92cc392fab285ad
Jade Stewart <jadekstewart3@gmail.com>
2024-02-05 17:49:14 -0700

    Notes and example on throwing and catching errors

--- 12_chapter/sandbox.js
+++ 12_chapter/sandbox.js
@@ -102,13 +102,17 @@
 //async and await
 
 const getTodos = async () => {
- const response = await fetch("todos/luigi.json");
+ const response = await fetch("todos/luigis.json");
+ if(response.status !== 200){
+  throw new Error("cannot fetch the data");
+ }
   const data = await response.json();
   
   return data;
 };
 
-getTodos().then(data => console.log("resolved:", data));
+getTodos().then(data => console.log("resolved:", data))
+.catch(err => console.log("rejected:", err.message));
 //async function always returns a promise
 //await can only be used inside an async function
 //await pauses the execution of the function until the promise is resolved

78c3a6c77c685b29aefb16df799b328dd7a416b5
Jade Stewart <jadekstewart3@gmail.com>
2024-02-09 10:47:38 -0700

    Almost done with api call

--- 13_chapter/weather_app/scripts/app.js
+++ 13_chapter/weather_app/scripts/app.js
@@ -1,2 +1,27 @@
 //this file is for DOM manipulation and event handling
 //kind of like a controller file
+const cityForm = document.querySelector('form');
+
+const updateCity = async (city) => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+
+  return { 
+    cityDetails: cityDetails, 
+    weather: weather
+  };
+};
+
+cityForm.addEventListener('submit', e => {
+  //prevent default action
+  e.preventDefault();
+  // get city value
+  const city = cityForm.city.value.trim();
+
+  //reset form
+  cityForm.reset();
+
+  //update UI with new city
+  updateCity(city).then(data => console.log(data))
+  .catch(err => console.log(err));
+});

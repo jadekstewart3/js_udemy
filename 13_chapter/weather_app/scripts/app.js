582bf02257fed08f1d8c890b7d2ec4a6e1df0d1f
Jade Stewart <jadekstewart3@gmail.com>
2024-02-13 15:02:55 -0700

    Add conditional logic to display corresponding night and day images

--- 13_chapter/weather_app/scripts/app.js
+++ 13_chapter/weather_app/scripts/app.js
@@ -20,6 +20,16 @@ const updateUI = data => {
       <span>&deg;C</span>
     </div>
   `;
+
+  // update night/day icons and images
+  let timeSrc = null;
+  if(weather.IsDayTime){
+    timeSrc = "img/day.svg";
+  } else {
+    timeSrc = "img/night.svg";
+  }
+  time.setAttribute("src", timeSrc);
+
   //remove d-none class if present 
   if(card.classList.contains("d-none")){
     card.classList.remove("d-none");

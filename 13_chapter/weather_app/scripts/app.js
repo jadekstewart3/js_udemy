685fd6b6c0070564f1a89bd8b0db14dd2195b0d4
Jade Stewart <jadekstewart3@gmail.com>
2024-02-13 14:57:25 -0700

    Create constants to update icon

--- 13_chapter/weather_app/scripts/app.js
+++ 13_chapter/weather_app/scripts/app.js
@@ -3,6 +3,8 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector(".card");
 const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
 
 
 const updateUI = data => {

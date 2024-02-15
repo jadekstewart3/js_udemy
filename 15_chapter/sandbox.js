ef38886a8edab4ac898ecc60dc5492bc834b4585
Jade Stewart <jadekstewart3@gmail.com>
2024-02-15 10:55:07 -0700

    Smol notes on object literal notation

---
+++ 15_chapter/sandbox.js
@@ -0,0 +1,22 @@
+//What is OOps in JS?
+//object in JS is like an object in Ruby
+//primitive data types get wrapped in an object to use methods
+
+//object literal notation
+
+//literal way to create an object
+const userOne = {
+  username: 'ryu',
+  email: 'ryu@gmail.com',
+  login(){
+    console.log('the user logged in');
+  },
+  logout(){
+    console.log('the user logged out');
+  }
+};
+
+console.log(userOne.email, userOne.username);
+userOne.login();
+
+//would be easier to use a constructor function

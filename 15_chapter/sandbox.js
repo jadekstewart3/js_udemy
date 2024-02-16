0f9f2584579e82d700460ef2e097ac7174d813f2
Jade Stewart <jadekstewart3@gmail.com>
2024-02-16 10:39:47 -0700

    Go over constructors

--- 15_chapter/sandbox.js
+++ 15_chapter/sandbox.js
@@ -5,22 +5,33 @@
 //object literal notation
 
 //literal way to create an object
-const userOne = {
-  username: 'ryu',
-  email: 'ryu@gmail.com',
-  login(){
-    console.log('the user logged in');
-  },
-  logout(){
-    console.log('the user logged out');
-  }
-};
+// const userOne = {
+//   username: 'ryu',
+//   email: 'ryu@gmail.com',
+//   login(){
+//     console.log('the user logged in');
+//   },
+//   logout(){
+//     console.log('the user logged out');
+//   }
+// };
 
-console.log(userOne.email, userOne.username);
-userOne.login();
+// console.log(userOne.email, userOne.username);
+// userOne.login();
 
 //would be easier to use a constructor function
 
 //Classes
 //class is like a blueprint for an object - describes all the basic properties and functionality that an object should have
 //object is an instance of a class
+
+
+//classes
+
+class User {
+  constructor(username, email){
+    //set up properties
+    this.username = username;
+    this.email = email;
+  }
+}

b92afc84f56413bd6ae1bf13252916e380fc4383
Jade Stewart <jadekstewart3@gmail.com>
2024-02-26 14:38:49 -0700

    Intro to firebase notes

---
+++ 16_chapter/sandbox.js
@@ -0,0 +1,8 @@
+//NOSQL database
+// firebase hosted by google
+// - collections
+// - documents
+// - properties
+// noSQL database consists of a database instance
+//- that holds collections
+//- that holds documents many documents of the JSON variety

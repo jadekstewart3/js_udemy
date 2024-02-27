f7e238e9e4945b0f02affe94e86bd7950255a1c4
Jade Stewart <jadekstewart3@gmail.com>
2024-02-27 08:26:18 -0700

    Some more noSQL notes

--- 16_chapter/sandbox.js
+++ 16_chapter/sandbox.js
@@ -4,5 +4,8 @@
 // - documents
 // - properties
 // noSQL database consists of a database instance
-//- that holds collections
-//- that holds documents many documents of the JSON variety
+// - that holds collections
+// - that holds documents many documents of the JSON variety
+// - that holds properties
+// - that holds values
+// - that holds subcollections

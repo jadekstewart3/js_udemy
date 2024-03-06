d7d726c45ae41588263f9224fec7b607673657d3
Jade Stewart <jadekstewart3@gmail.com>
2024-03-06 16:13:47 -0700

    Continue connecting firebase

--- 17_chapter/chat_project/src/index.js
+++ 17_chapter/chat_project/src/index.js
@@ -1,1 +1,22 @@
-console.log("helloooooo!");
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6IiTwwYixLUzxgydsy7MsWe31zynIX6Y",
+  authDomain: "udemy-modern-javascript-dc88b.firebaseapp.com",
+  projectId: "udemy-modern-javascript-dc88b",
+  storageBucket: "udemy-modern-javascript-dc88b.appspot.com",
+  messagingSenderId: "229251160118",
+  appId: "1:229251160118:web:a0241af6c1eebb328e2097",
+  measurementId: "G-K9MQR8623G"
+};
+
+
+// connect to firestore
+initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore(); 
+
+//collection reference
+const chatsRef = collection(db, 'chats');
